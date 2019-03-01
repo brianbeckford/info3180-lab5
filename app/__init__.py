5c909c0fadcb36481b46ff875adf4aac96ce3f52
Brian Beckford <brian.beckford@mymona.uwi.edu>
2019-03-01 03:19:45 +0000

    info3180-lab5

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://brianbeckford:beckford@localhost/lab5"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
